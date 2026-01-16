bec60155064cac48dbe1bcc7976438abcd3ac239
veereshn696-alt <veereshn696@gmail.com>
2026-01-16 15:57:19 +0530

    Update kilometer.py

--- kilometer.py
+++ kilometer.py
@@ -3,7 +3,7 @@ if len(sys.argv)<2:
     print("argument error")
     miles=11
 else:
-    miles=int(sys.argv[1])
-kilometers=miles*1.6
-print("miles=",miles)
-print("kilometers=",kilometers)
+ miles=int(sys.argv[1])
+ kilometers=miles*1.6
+ print("miles=",miles)
+ print("kilometers=",kilometers)

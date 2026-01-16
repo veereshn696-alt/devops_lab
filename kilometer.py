e5492e273bb59280517d0cfec175f476669b7860
veereshn696-alt <veereshn696@gmail.com>
2026-01-16 15:59:56 +0530

    Update kilometer.py

--- kilometer.py
+++ kilometer.py
@@ -2,9 +2,8 @@ import sys
 if len(sys.argv)<2:
     print("argument error")
     miles=11
-    kilometers=miles*1.6
 else:
  miles=int(sys.argv[1])
- kilometers=miles*1.6
- print("miles=",miles)
- print("kilometers=",kilometers)
+kilometers=miles*1.6
+print("miles=",miles)
+print("kilometers=",kilometers)
